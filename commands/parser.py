02203e53b81d38e4ad07480ae6c360022bb6cf82
Cheaterman <the.cheaterman@gmail.com>
2015-11-08 16:32:47 +0100

    Made CommandParser more Pythonic

--- commands/parser.py
+++ commands/parser.py
@@ -2,17 +2,18 @@ import re
 
 
 class CommandParser(object):
-    commands = {}
+    def __init__(self):
+        self.commands = []
 
     def command(self, pattern):
         regexp = re.compile(pattern)
         def command_generator(function):
-            self.commands[regexp] = function
+            self.commands.append((regexp, function))
             return function
         return command_generator
 
     def parse(self, bot, channel, user, msg):
-        for regexp, callback in self.commands.items():
+        for regexp, callback in self.commands:
             match = regexp.match(msg)
             if match:
                 if not callback(bot, channel, user, *match.groups()):
