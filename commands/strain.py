ba9cf54510de57f2bf49e28daabec947931848a2
The Cheaterman <the.cheaterman@gmail.com>
2021-07-25 15:41:06 +0200

    Ditto but properly

--- commands/strain.py
+++ commands/strain.py
@@ -4,11 +4,14 @@ from .leaflib import search
 
 @bot.command('!strain (.*)')
 def strain(self, channel, user, name):
-    data = search(name)
     say = lambda msg: self.say(channel, msg, user)
-    descriptions = [strain.description for strain in data]
+    descriptions = [
+        strain.description
+        for strain in search(name)
+        if strain.description
+    ]
 
-    if not any(descriptions):
+    if not descriptions:
         say(f'Sorry, strain "{name}" not found :-S')
         return
 
